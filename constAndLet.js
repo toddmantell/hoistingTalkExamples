a447073811c78dc2245efa613b36ec958a7af697
todder02 <tmantell99@yahoo.com>
2018-10-22 19:30:26 -0500

    clarified examples

--- constAndLet.js
+++ constAndLet.js
@@ -1,8 +1,18 @@
-console.log(tickerSymbol);
+//1. const throws ReferenceError
+// console.log(tickerSymbol);
 
-const tickerSymbol = 'MSFT';
-
+// const tickerSymbol = 'MSFT';
 
 //This really is:
 //const tickerSymbol;
 //console.log(tickerSymbol)
+
+//2. let unassigned will be undefined (like var)
+// let tickerSymbol;
+
+// console.log({tickerSymbol});
+
+//2. let "hoisted" will throw a RefError because of the TDZ
+console.log({tickerSymbol});
+
+let tickerSymbol = 'LKQ';
--- withLexicalScope.js
+++ withLexicalScope.js
@@ -2,12 +2,9 @@ outerScope = 'outer scope';
 
 var outerScope; //if it is a let, it will throw a referenceError because let and const work differently than var
 
-console.log(outerScope);
-
 override();
 
-var override = function override() {
-  //outerScope is in override's lexical scope
+function override() {
   console.log(outerScope);
   var outerScope = 'inner scope';
 }
