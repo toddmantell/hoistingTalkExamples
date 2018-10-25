19154e2301bbe398b859a557c9648b00f2ef9bb4
todder02 <tmantell99@yahoo.com>
2018-10-25 10:15:56 -0500

    renamed files for easier location during presentation

--- 1basicHoisting.js
+++ 1basicHoisting.js
@@ -2,12 +2,10 @@
 
 console.log(stockQuote);
 
-stockQuote = {
+var stockQuote = {
 	name: 'Microsoft', 
 	ticker: 'MSFT',
 	exchange: 'NYSE',
 	lastPrice: 109.97,
 	volume: 20000000
 };
-
-var stockQuote;
